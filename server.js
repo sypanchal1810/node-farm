25a1540b0f69b3c358e0e757c4948676498cb506
Saurabh Panchal <saurabh.panchal1018@gmail.com>
2024-04-02 15:28:45 +0530

    Minor Fixes

--- server.js
+++ server.js
@@ -1,51 +1,21 @@
-const dotenv = require('dotenv');
 const fs = require('fs');
 const http = require('http');
-const https = require('https');
 const path = require('path');
 const slugify = require('slugify');
 const url = require('url');
 
 const replaceTemplate = require('./modules/replaceTemplate');
-
-dotenv.config({ path: './config.env' });
-
 const publicPath = path.join(__dirname, 'templates');
 
 const getFileData = (fileName) => {
   return path.join(publicPath, fileName);
 };
 
-//////////////////////////////////////////////////
 // Building Simple API
 
 const tempCard = fs.readFileSync(getFileData('template-card.html'), 'utf-8');
 const tempOverview = fs.readFileSync(getFileData('template-overview.html'), 'utf-8');
 const tempProduct = fs.readFileSync(getFileData('template-product.html'), 'utf-8');
-
-// const fileUrl = new URL('file://github.com/sypanchal1810/node-farm/blob/master/templates/template-card.html');
-
-// const tempCardurl = fs.readFileSync(fileUrl, 'utf-8');
-
-// const giturl = 'https://raw.githubusercontent.com/sypanchal1810/node-farm/master/templates/template-card.html';
-
-// https
-//   .get(giturl, (res) => {
-//     let data = '';
-
-//     res.on('data', (chunk) => {
-//       data += chunk;
-//     });
-
-//     res.on('end', () => {
-//       console.log(data);
-//     });
-//   })
-//   .on('error', (err) => {
-//     console.log('Error: ', err.message);
-//   });
-
-// console.log(tempCardurl);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -94,7 +64,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const port = process.env.PORT || 3000;
+const port = 8000;
 server.listen(port, '0.0.0.0', () => {
   console.log('Listening to the request on port 8000');
 });
